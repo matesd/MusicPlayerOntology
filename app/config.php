0f0d194aeac375737f4f06ac5c7f939a603ff8ed
Martin Doubravsky <martin.doubravsky@gmail.com>
2009-12-21 22:12:57 +0100

    db config

---
+++ app/config.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once(dirname(__FILE__). '/../libs/arc/ARC2.php');
+
+// SQL database configuration for storing the postings:
+$arc_config = array(
+  /* MySQL db settings */
+    
+  'db_host' => 'localhost',
+  'db_user' => 'mpo',
+  'db_pwd' => 'mpo',
+  'db_name' => 'mpo',
+
+  /* ARC2 store settings */
+  'store_name' => 'mpo',
+  
+  /* SPARQL endpoint settings */
+  'endpoint_features' => array(
+    'select', 'construct', 'ask', 'describe', 
+    'load', 'insert', 'delete', 
+    'dump' /* dump is a special command for streaming SPOG export */
+  ),
+  'endpoint_timeout' => 60, /* not implemented in ARC2 preview */
+  'endpoint_read_key' => '', /* optional */
+  'endpoint_write_key' => '', /* optional */
+  'endpoint_max_limit' => 250, /* optional */
+);
